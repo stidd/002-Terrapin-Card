b5ee50943716876b233b27d9c558ebc59b40981e
Steven Tidd <steven.tidd@gmail.com>
2020-10-13 07:20:17 -0400

    fixed tests

--- src/test/java/TerrapinCardTest.java
+++ src/test/java/TerrapinCardTest.java
@@ -1,70 +1,70 @@
-//import org.junit.Test;
-//
-//import static org.junit.Assert.*;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.Test;
 
-class TerrapinCardTest {
+//import static org.junit.jupiter.api.Assertions.*;
+
+public class TerrapinCardTest {
 
     @Test
-    void getBalance() {
+    public void getBalance() {
         TerrapinCard billy = new TerrapinCard(12);
-        assertEquals(billy.getBalance(), 12.0);
+        assertEquals(billy.getBalance(), 12.0, 0.001);
     }
 
     @Test
-    void payEconomical() {
+    public void payEconomical() {
         TerrapinCard billy = new TerrapinCard(10);
         billy.payEconomical();
         billy.payEconomical();
-        assertEquals(billy.getBalance(), 5.0);
+        assertEquals(billy.getBalance(), 5.0, 0.001);
     }
 
     @Test
-    void payEconomicalNonNegative() {
+    public void payEconomicalNonNegative() {
         TerrapinCard billy = new TerrapinCard(1);
         billy.payEconomical();
-        assertEquals(billy.getBalance(), 1.0);
+        assertEquals(billy.getBalance(), 1.0, 0.001);
     }
 
 
 
     @Test
-    void payGourmet() {
+    public void payGourmet() {
         TerrapinCard billy = new TerrapinCard(10);
         billy.payEconomical();
         billy.payGourmet();
-        assertEquals(billy.getBalance(), 3.5);
+        assertEquals(billy.getBalance(), 3.5, 0.001);
     }
 
     @Test
-    void payGourmetNonNegative() {
+    public void payGourmetNonNegative() {
         TerrapinCard billy = new TerrapinCard(7.99);
         billy.payGourmet();
         billy.payGourmet();
-        assertEquals(billy.getBalance(), 3.99);
+        assertEquals(billy.getBalance(), 3.99, 0.001);
     }
 
     @Test
-    void loadMoney() {
+    public void loadMoney() {
         TerrapinCard billy = new TerrapinCard(20);
         billy.payEconomical();
         billy.payGourmet();
         billy.loadMoney(30);
-        assertEquals(billy.getBalance(), 43.5);
+        assertEquals(billy.getBalance(), 43.5, 0.001);
     }
 
     @Test
-    void loadMoneyMax() {
+    public void loadMoneyMax() {
         TerrapinCard billy = new TerrapinCard(100);
         billy.loadMoney(100);
-        assertEquals(billy.getBalance(), 150.0);
+        assertEquals(billy.getBalance(), 150.0, 0.001);
     }
 
     @Test
-    void toStringTest() {
+    public void toStringTest() {
         TerrapinCard billy = new TerrapinCard(100);
         billy.loadMoney(20);
         assertEquals(billy.toString(), "The card has " + billy.getBalance() + " dollars");
